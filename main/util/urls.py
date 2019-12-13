275ef90950f418426f734b9e157738588324b82d
Martin Wong <martinwong327@gmail.com>
2019-12-14 04:00:01 +0800

    add asgi webserver

--- main/util/urls.py
+++ main/util/urls.py
@@ -12,14 +12,14 @@ def get_url_patterns() -> list:
         path('', debug.default_urlconf),
         path('admin/', admin.site.urls),
         path('accounts/login/', LoginView.as_view(
-            template_name='admin/login.html',
+            template_name='/admin/login.html',
             extra_context={
                 'site_header': 'User Login',
             }
         )),
         path('accounts/logout/', LogoutView.as_view()),
         path('api/swagger/', swagger_view.with_ui('swagger', cache_timeout=0)),
-        path('api/redoc/', swagger_view.with_ui('redoc', cache_timeout=0))
+        # path('api/redoc/', swagger_view.with_ui('redoc', cache_timeout=0))
     ]
 
     class_view_list = [
