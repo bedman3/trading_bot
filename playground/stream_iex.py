ad910758f55cbfa90d0f6573f476897d53aa6bc3
Martin Wong <martinwong327@gmail.com>
2019-12-20 00:50:44 +0800

    add rabbitmq connection

--- playground/stream_iex.py
+++ playground/stream_iex.py
@@ -1,14 +1,25 @@
 from websocket import create_connection
-import simplejson as json
+import json
+import pika
+
+from pika.channel import Channel
 
 if __name__ == '__main__':
     ws = create_connection("wss://api.tiingo.com/iex")
 
+    # connection = pika.BlockingConnection(pika.ConnectionParameters('amqp://martin:martin@vpn.desktop.martinwongsk.com:5672/'))
+
+    credentials = pika.PlainCredentials('martin', 'martin')
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('vpn.desktop.martinwongsk.com', 5672, '/', credentials))
+    channel: Channel = connection.channel()
+    channel.queue_declare('')
+
     subscribe = {
-            'eventName':'subscribe',
-            'authorization':'90818697771bcf20991692e3e1541cb8c2fbe7aa',
-            'eventData': {
-                'thresholdLevel': 0
+        'eventName': 'subscribe',
+        'authorization': '90818697771bcf20991692e3e1541cb8c2fbe7aa',
+        'eventData': {
+            'thresholdLevel': 0
         }
     }
 
@@ -19,4 +30,3 @@ if __name__ == '__main__':
             if 'data' in recv_wss:
                 f.write(f'{recv_wss}\n')
                 print(recv_wss)
-                
